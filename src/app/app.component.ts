ec02534660494ec99d05f72ef4521e365f82c1f6
snow <snowyle22@gmail.com>
2024-10-25 15:10:38 +0200

    Ajout du footer et du header comme component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   standalone: true
 })
 
